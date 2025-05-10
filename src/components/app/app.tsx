0fb13712c1be4c97833b51bf32b9f50ae389fe78
lenaa1 <85735585+lenaa1@users.noreply.github.com>
2025-05-11 01:24:03 +0500

    routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,36 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword,
+  Feed
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
+import { Routes, Route } from 'react-router-dom';
 
 import { AppHeader } from '@components';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/reset-password' element={<ResetPassword />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/profile/oredrs' element={<ProfileOrders />} />
+
+      <Route path='/*' element={<NotFound404 />} />
+    </Routes>
   </div>
 );
 
